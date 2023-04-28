f1819f7ce1589bd98cbedcaeeb8b11f1343ff6d9
orlando.patriarcha <orlando.patriarcha@gmail.com>
2023-04-28 08:13:39 -0300

    adicao de classe main para pacote RMI e objeto Person

---
+++ sockets/src/main/java/org/sd/rmi/methodInvocation/Main.java
@@ -0,0 +1,53 @@
+package org.sd.rmi.methodInvocation;
+
+import java.rmi.AccessException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author orlando
+ */
+public class Main {
+
+    PersonGatewayService service;
+
+    public void startServer() {
+        try {
+            this.service = new PersonGatewayServiceImpl();
+            this.service.createStubAndBind();
+        } catch (RemoteException e) {
+            System.out.println("Exception Occurred: " + e);
+        }
+    }
+
+    public void executeClienteInvocation() {
+        Registry registry;
+        try {
+            registry = LocateRegistry.getRegistry();
+            PersonGatewayService server = (PersonGatewayService) registry.lookup("PersonGatewayService");
+            final String PERSON_KEY = "1";
+
+            PersonHolder holder = new PersonHolder();
+
+            Person result = server.retrievePerson(PERSON_KEY);
+
+            System.out.println("Resultado da invocação:" + result);
+        } catch (RemoteException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NotBoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.startServer();
+        main.executeClienteInvocation();
+        System.exit(0);
+    }
+}
--- sockets/src/main/java/org/sd/rmi/methodInvocation/PersonGatewayService.java
+++ sockets/src/main/java/org/sd/rmi/methodInvocation/PersonGatewayService.java
@@ -10,4 +10,7 @@ import java.rmi.RemoteException;
 public interface PersonGatewayService extends Remote {
 
 	public Person retrievePerson(String thePerson) throws RemoteException;
+        
+        
+        public void createStubAndBind() throws RemoteException;
 }
